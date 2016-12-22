d5d7677a8d14f39cfbccb0bbc03a92a0dda70db0
Cristian Jaramillo <cristian_gerar@hotmail.com>
2016-12-21 20:03:32 -0600

    connection db oracle test

--- src/Application.php
+++ src/Application.php
@@ -9,7 +9,13 @@ use DHL\Asterisk\{AGI, AGI_AsteriskManager};
  */
 class Application
 {
-	
+
+	/**
+	 *
+	 * @var Connection Oracle DB
+	 */
+	private $connection;
+
 	/**
 	 *
 	 *
@@ -24,7 +30,35 @@ class Application
 	 */
 	public function __construct()
 	{
+
 		$this->agi = new AGI();
+
+		$database = (object) include 'database.php';
+
+		/**
+		 |
+		 | Creamos la conexión
+		 |
+		*/
+		$this->connection = oci_connect(
+				$database->username, 
+				$database->password, 
+				build_connection_string($database)
+			);
+
+		/**
+		 |
+		 | Validamos la connexión
+		 |
+		 */
+		if (!$this->connection) {
+		   $message_error = oci_error();
+		   $this->agi->verbose($message_error['message']);
+		   exit;
+		} else {
+			$this->agi->verbose("Connected to Oracle!");
+		}
+
 	}
 
 	/**
@@ -33,8 +67,21 @@ class Application
 	 */
 	public function delivery()
 	{
-		$this->agi->verbose('verbose', 0);
 		$this->agi->verbose(print_r($this->agi, true));
+	}
+
+	/**
+	 *
+	 * 
+	 */
+	public function close()
+	{
+		/**
+		 |
+		 | Close the Oracle connection
+		 |
+		 */
+		oci_close($this->connection);
 	}
 
 }
--- src/helpers.php
+++ src/helpers.php
@@ -1,2 +1,51 @@
 <?php
 
+if(!function_exists('build_connection_string'))
+{
+
+	/**
+	 |
+	 | Construye una cadena de conexión con el siguiente formato:
+	 | //[host]:[port]/[database]
+	 |
+	 */
+	function build_connection_string($config)
+	{
+
+		$connection_string = "//";
+
+		if(is_null($config) || !is_object($config))
+		{
+			return null;
+		}
+		
+		$connection_string .= is_empty_or_null($config->host) ?
+			'localhost' : $config->host;
+
+		$connection_string .= ":";
+
+		$connection_string .= is_empty_or_null($config->port) ?
+			'1521' : $config->port;
+
+		$connection_string .= "/";
+
+		$connection_string .= is_empty_or_null($config->database) ?
+			'ocrl' : $config->database;
+
+		return $connection_string;
+	}
+}
+
+
+if(!function_exists('is_empty_or_null'))
+{
+	/**
+	|
+	| Identifica si una cadena es nulla o vacia
+	|
+	*/
+	function is_empty_or_null($property)
+	{
+		return is_null($property) || empty($property);
+	}
+}
